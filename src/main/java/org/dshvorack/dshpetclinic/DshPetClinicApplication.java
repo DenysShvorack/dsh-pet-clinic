b507fe5a3c270281c9d93deb6b0232f979d6bd5c
dshvorack <dshvorack@scope.ch>
2018-10-21 18:52:34 +0300

    Initial Project Add

---
+++ src/main/java/org/dshvorack/dshpetclinic/DshPetClinicApplication.java
@@ -0,0 +1,12 @@
+package org.dshvorack.dshpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DshPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DshPetClinicApplication.class, args);
+    }
+}
